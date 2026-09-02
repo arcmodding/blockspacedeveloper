df64ffed794a02771cf387bf549d653c9a580316
arc <mosteatable@gmail.com>
2026-09-01 20:21:40 -0400

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"BlockWorld",description:"A Roblox-inspired social gaming hub"};
+export default function RootLayout({children}){return <html><body>{children}</body></html>}
