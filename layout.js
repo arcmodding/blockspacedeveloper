fcd60a9451020bed996ab9f5655d5a1f6c1e76c0
arc <mosteatable@gmail.com>
2026-09-01 20:20:20 -0400

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"BlockWorld",description:"A Roblox-inspired social gaming hub"};
-export default function RootLayout({children}){return <html><body>{children}</body></html>}
